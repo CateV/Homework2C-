58672b4f650420dc8cea750a714b9ff9eb138069
Kate V <katsiarynait@gmail.com>
2023-01-22 02:59:00 +0300

    готово, +в коде один вопрос

---
+++ Task1(SecondDigit)/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 1: Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+456 -> 5
+782 -> 8
+918 -> 1 */
+int InputStringToInt(string message)
+{
+    Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+int number = InputStringToInt("Введите 3-значное число");
+if(number<0) number=-number;
+int FindPenaltDigit(int a)
+{
+    int b = a / 10;
+    int penalt = b % 10;
+    return penalt;
+}
+/*А ещё можно было вот так:
+int penalt=a%100/10;
+return penalt
+}
+Кстати, как лучше для производительности?*/
+if (number < 100 || number > 999)
+{
+    System.Console.WriteLine($"число {number} - не трёхзначное");
+}
+else
+{
+    System.Console.WriteLine($"Вторая цифра числа {number} равна {FindPenaltDigit(number)}");
+}
