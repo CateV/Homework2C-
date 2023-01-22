69de34a3e9ea8c0dece0dcb496a677ccc62aaa9b
Kate V <katsiarynait@gmail.com>
2023-01-22 05:51:20 +0300

    решала не подглядывая в разбор домашек =)

---
+++ Task4(DeleteSecondDigit)/Program.cs
@@ -0,0 +1,6 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число 
+и удаляет вторую цифру этого числа */
+int number = new Random().Next(100,1000);
+int withoutDozens = number - number%100 + number%10;
+int hundredsToDozens = withoutDozens%10 + withoutDozens/10;
+System.Console.WriteLine($"Было число {number}, стало число {hundredsToDozens}");
