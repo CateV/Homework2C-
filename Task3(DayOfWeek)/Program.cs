75572174f33bdfabbe3de404ff6d1f5e11e54d16
Kate V <katsiarynait@gmail.com>
2023-01-22 05:31:15 +0300

    Владимир, ответьте, пожалуйста, на мои вопросы в комментариях

---
+++ Task3(DayOfWeek)/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+6 -> да 
+7 -> да 
+1 -> нет */
+int InputStringToInt(string message)
+{
+    Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+int dayNumber = InputStringToInt("Введите число от 1 до 7");
+string[] weekDay = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+int index = dayNumber - 1;
+if (dayNumber >0 && dayNumber <=7)
+{    string status;
+    if (dayNumber > 5)
+        status = "выходной день";
+    else
+        status = "не выходной день";
+   System.Console.WriteLine(weekDay[index] + "-" + status);
+}
+else 
+{
+       System.Console.WriteLine("нет такого дня недели");
+}
+//*стоит ли здесь создавать функцию DayOff, если можно решить намного проще?
+/*пробовала писать: 
+if (dayNumber = 6 || dayNumber = 7)...
+получила ошибку:
+"Оператор "||" невозможно применить к операнду  
+типа "int" и "int""
+Как это понимать? Неужели правда невозможно использовать с int и int??? */
